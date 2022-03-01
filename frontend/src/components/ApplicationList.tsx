0a294052cabf8022c4fc52ab89f52af733280d48
HenrikVL <henrik.viken.lied@gmail.com>
2022-03-01 15:53:22 +0100

    style: prettier formatting

--- frontend/src/components/ApplicationList.tsx
+++ frontend/src/components/ApplicationList.tsx
@@ -1,36 +1,28 @@
-import { Container, createStyles } from "@mantine/core";
-import ApplicationItem from "./ApplicationItem";
-import ApplicationI from "../types/application";
+import { Container, createStyles } from '@mantine/core'
+import ApplicationItem from './ApplicationItem'
+import ApplicationI from '../types/application'
 
-let applications = require('../fixtures/applications.json');
+let applications = require('../fixtures/applications.json')
 
-let formatted = applications.map((item : ApplicationI, idx : number) => {
-    return (
-        <ApplicationItem key={idx} {...item} />
-    )
+let formatted = applications.map((item: ApplicationI, idx: number) => {
+	return <ApplicationItem key={idx} {...item} />
 })
 
 const useStyles = createStyles((theme) => ({
-    container: {
-      maxWidth: "600px",
-      minWidth: "300px",
-      display: "flex",
-      justifyContent: "space-between",
-      flexDirection: "column",
-      gap: "1rem",
-      margin: "auto",
-    },
-  }));
-
-
+	container: {
+		maxWidth: '600px',
+		minWidth: '250px',
+		display: 'flex',
+		justifyContent: 'space-between',
+		flexDirection: 'column',
+		gap: '1rem',
+		margin: 'auto',
+	},
+}))
 
 function ApplicationList() {
-    const { classes } = useStyles();
-    return (
-      <Container className={classes.container} >
-		    {formatted}
-	    </Container>
-    )
-};
+	const { classes } = useStyles()
+	return <Container className={classes.container}>{formatted}</Container>
+}
 
-export default ApplicationList;
+export default ApplicationList
