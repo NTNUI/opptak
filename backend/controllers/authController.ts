d000a92df83770d150e119f5889ac6a88f5c25bd
LiviaValenti <54808860+LiviaValenti@users.noreply.github.com>
2022-03-11 14:55:02 +0100

    refactor: error messages
    Co-authored-by: Mats Finsås <xtrah@live.com>

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -38,7 +38,7 @@ async function getCommittees() {
 	return CommitteeModel.find()
 		.then((committees) => committees)
 		.catch(() => {
-			throw new CustomError('Could not get committees from local db', 500)
+			throw new CustomError('Could not retrieve committees from local database', 500)
 		})
 }
 
@@ -56,7 +56,7 @@ export async function refresh(req: Request, res: Response, next: NextFunction) {
 		if (tokens) {
 			return res.status(200).json({ access: tokens.access })
 		}
-		return res.status(401).json({ message: 'Token is invalid or expired' })
+		return res.status(401).json({ message: 'Refresh token is invalid or expired' })
 	} catch (error) {
 		return next(error)
 	}
