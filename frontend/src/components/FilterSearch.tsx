07e4b40cf8429af15f562f1d115bd4d6f7550608
HenrikVL <henrik.viken.lied@gmail.com>
2022-04-06 22:14:29 +0200

    feat: filter styling

--- frontend/src/components/FilterSearch.tsx
+++ frontend/src/components/FilterSearch.tsx
@@ -1,13 +1,26 @@
 import {
 	ActionIcon,
+	Badge,
+	Chip,
+	Chips,
 	Collapse,
 	createStyles,
 	Input,
 	MultiSelect,
+	NativeSelect,
 	Select,
+	Tooltip,
 } from '@mantine/core'
 import { useState } from 'react'
-import { Search, Adjustments } from 'tabler-icons-react'
+import {
+	Search,
+	Adjustments,
+	AlertCircle,
+	Circle,
+	Check,
+	LivePhoto,
+	X,
+} from 'tabler-icons-react'
 
 const useStyles = createStyles((theme) => ({
 	filterWrapper: {
@@ -71,8 +84,19 @@ const useStyles = createStyles((theme) => ({
 		//backgroundColor: theme.colors.ntnui_yellow[9],
 		//padding: '0.5rem',
 	},
-	multiselectLabel: {
-		//color: 'white',
+	selectLabel: {
+		color: 'black',
+	},
+	multiselectInput: {
+		background: 'transparent',
+		color: 'white',
+	},
+	selectInput: {
+		background: 'transparent',
+		color: 'white',
+	},
+	badgeLabel: {
+		color: 'white',
 	},
 	collapseInner: {
 		border: '2px solid' + theme.colors.ntnui_yellow[9],
@@ -83,11 +107,53 @@ const useStyles = createStyles((theme) => ({
 		gridTemplateColumns: '1fr 1fr',
 		marginBottom: '1rem',
 	},
+	filterPreview: {
+		display: 'flex',
+		flexDirection: 'row',
+		gap: '0.5rem',
+		width: '100%',
+		justifyContent: 'center',
+		marginBottom: '1rem',
+	},
+	filterChipsWrapper: {
+		display: 'flex',
+		gap: '0.5rem',
+	},
 }))
+
+const removeButton = (
+	<ActionIcon size='xs' color='yellow' radius='xl' variant='transparent'>
+		<X size={12} strokeWidth={3} />
+	</ActionIcon>
+)
+
+const filterBadge = (
+	<>
+		<Badge
+			color='yellow'
+			variant='outline'
+			sx={{ paddingRight: 3 }}
+			rightSection={removeButton}
+		>
+			Sprint
+		</Badge>
+		<Badge
+			color='yellow'
+			variant='outline'
+			sx={{ paddingRight: 3 }}
+			rightSection={removeButton}
+		>
+			Blits
+		</Badge>
+	</>
+)
 
 function FilterSearch() {
 	const { classes } = useStyles()
 	const [opened, setOpen] = useState(false)
+	const [committees, setCommittees] = useState<string[]>([])
+	const [status, setStatus] = useState<string>()
+	const [dateSort, setDateSort] = useState<string>()
 
 	return (
 		<div className={classes.filterWrapper}>
@@ -119,27 +185,47 @@ function FilterSearch() {
 				<div className={classes.collapseInner}>
 					<MultiSelect
 						classNames={{
-							label: classes.multiselectLabel,
+							label: classes.selectLabel,
+							input: classes.multiselectInput,
 						}}
-						variant='filled'
 						data={['Sprint', 'Hovedstyret', 'Koiene', 'Moment', 'Blits']}
-						label='Velg utvalg'
+						label={<span className={classes.badgeLabel}>Velg utvalg</span>}
 						placeholder='Velg utvalg'
 						searchable
+						clearable
 						nothingFound='Nothing found'
+						value={committees}
+						onChange={setCommittees}
 					/>
 					<Select
 						classNames={{
-							label: classes.multiselectLabel,
+							label: classes.selectLabel,
+							input: classes.selectInput,
 						}}
-						variant='filled'
 						data={['Alle', 'Akseptert', 'Innkalt', 'Ubehandlet', 'Avvist']}
-						label='Velg status'
-						placeholder='Velg status'
-						nothingFound='Nothing found'
+						label={<span className={classes.badgeLabel}>Velg status</span>}
+						defaultValue={'Alle'}
+						value={status}
+						onChange={(e) => setStatus(e as string)}
 					/>
 				</div>
 			</Collapse>
+			<div className={classes.filterPreview}>
+				<div className={classes.filterChipsWrapper}>{filterBadge}</div>
+				<Select
+					classNames={{
+						label: classes.selectLabel,
+						input: classes.selectInput,
+					}}
+					data={['Nyeste først', 'Eldste først']}
+					//label={<span className={classes.badgeLabel}>Sorter etter</span>}
+					//nothingFound='Nothing found'
+					defaultValue={'Nyeste først'}
+					size='xs'
+					value={dateSort}
+					onChange={(e) => setDateSort(e as string)}
+				/>
+			</div>
 		</div>
 	)
 }
---
+++ frontend/src/utils/status.ts
@@ -0,0 +1,10 @@
+export enum StatusTypes {
+	PENDING = 'Pending',
+	INVITED_TO_INTERVIEW = 'Invited to interview',
+	INTERVIEW_DECLINED = 'Interview declined',
+	INTERVIEW_COMPLETED = 'Interview completed',
+	OFFER_GIVEN = 'Offer given',
+	OFFER_DECLINED = 'Offer declined',
+	ACCEPTED = 'Accepted',
+	REJECTED = 'Rejected',
+}
