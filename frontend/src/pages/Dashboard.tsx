dede48e8cec4446e2ebfa623d97c01556fb1202e
supapesh <peshwasm12@gmail.com>
2022-04-06 15:38:56 +0200

    chore: run fix

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,11 @@
 import { Box, createStyles } from '@mantine/core'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { CalendarEvent, FileText, Lock, Users } from 'tabler-icons-react'
-import { isApplicationPeriodActive, getAdmissionPeriod } from '../services/Applications'
-
+import { CalendarEvent, FileText, Users } from 'tabler-icons-react'
+import {
+	isApplicationPeriodActive,
+	getAdmissionPeriod,
+} from '../services/Applications'
 
 const useStyles = createStyles((theme) => ({
 	dashboardWrapper: {
@@ -18,7 +20,7 @@ const useStyles = createStyles((theme) => ({
 		margin: 0,
 		height: '100%',
 		display: 'flex',
-		flexDirection:'row',
+		flexDirection: 'row',
 		gap: '1.5rem',
 		alignItems: 'center',
 		color: theme.colors.ntnui_yellow[9],
@@ -38,21 +40,21 @@ const useStyles = createStyles((theme) => ({
 		padding: '1rem',
 		cursor: 'pointer',
 		'&:active': {
-			transform: 'scale(0.98)'
+			transform: 'scale(0.98)',
 		},
 	},
 	date: {
 		color: 'royalblue',
-	}
+	},
 }))
 
 function Dashboard() {
 	const { classes } = useStyles()
 	const navigate = useNavigate()
 	const [periodOpen, setPeriodOpen] = useState<boolean>(false)
-	const [startDate, setStartDate] = useState<string>("")
-	const [endDate, setEndDate] = useState<string>("")
-	
+	const [startDate, setStartDate] = useState<string>('')
+	const [endDate, setEndDate] = useState<string>('')
+
 	useEffect(() => {
 		const getApplicationPeriodActiveAsync = async () => {
 			try {
@@ -69,7 +71,8 @@ function Dashboard() {
 							year: 'numeric',
 						})
 						const parsedStartDate = new Date(
-							response.admissionPeriod.start_date).toLocaleDateString('no-No', {
+							response.admissionPeriod.start_date
+						).toLocaleDateString('no-No', {
 							month: 'long',
 							day: '2-digit',
 							year: 'numeric',
@@ -89,21 +92,37 @@ function Dashboard() {
 	return (
 		<Box className={classes.dashboardWrapper}>
 			<h1>Hei, Bolle Bollesen!</h1>
-			{periodOpen ? (<p>Opptaksperioden er satt fra <span className={classes.date}>{startDate}</span> til <span className={classes.date}>{endDate}</span> </p>) : (<p>Det er for tiden ingen satt opptaksperiode</p>)}
+			{periodOpen ? (
+				<p>
+					Opptaksperioden er satt fra{' '}
+					<span className={classes.date}>{startDate}</span> til{' '}
+					<span className={classes.date}>{endDate}</span>{' '}
+				</p>
+			) : (
+				<p>Det er for tiden ingen satt opptaksperiode</p>
+			)}
 			<div className={classes.metroBoxWrapper}>
-			<Box className={classes.metroBoxes} onClick={() => navigate('/applications')}>
-				<FileText size={150} strokeWidth={0.7} /> Søknader 
-			</Box>
-			<Box className={classes.metroBoxes} onClick={() => navigate('/admission-status')}>
-				<Users size={150} strokeWidth={0.7} /> Opptaksstatus
-			</Box>
-			<Box className={classes.metroBoxes} onClick={() => navigate('/admission-period')}>
-				<CalendarEvent size={150} strokeWidth={0.7} /> Opptaksperiode
-			</Box>
+				<Box
+					className={classes.metroBoxes}
+					onClick={() => navigate('/applications')}
+				>
+					<FileText size={150} strokeWidth={0.7} /> Søknader
+				</Box>
+				<Box
+					className={classes.metroBoxes}
+					onClick={() => navigate('/admission-status')}
+				>
+					<Users size={150} strokeWidth={0.7} /> Opptaksstatus
+				</Box>
+				<Box
+					className={classes.metroBoxes}
+					onClick={() => navigate('/admission-period')}
+				>
+					<CalendarEvent size={150} strokeWidth={0.7} /> Opptaksperiode
+				</Box>
 			</div>
 		</Box>
 	)
 }
 
 export default Dashboard
-
