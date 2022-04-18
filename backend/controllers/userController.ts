0d98664682df9b24c82aa9ddfbdb8a9e51d19409
LiviaValenti <54808860+LiviaValenti@users.noreply.github.com>
2022-04-18 14:46:04 +0200

    feat: profile endpoint (#75)
    * profile endpoint
    * sample service for frontend

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -24,4 +24,26 @@ const getUserCommittees = async (
 	}
 }
 
-export default getUserCommittees
+const getUserProfile = async (
+	req: RequestWithNtnuiNo,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const { ntnuiNo } = req
+		if (!ntnuiNo) throw UnauthorizedUserError
+		const user = await UserModel.findOne({ _id: ntnuiNo }).populate(
+			'committees.committee'
+		)
+		if (!user) throw UnauthorizedUserError
+		return res.status(200).json({
+			first_name: user.first_name,
+			last_name: user.last_name,
+			committees: user.committees,
+		})
+	} catch (error) {
+		return next(error)
+	}
+}
+
+export { getUserCommittees, getUserProfile }
--- frontend/src/services/User.ts
+++ frontend/src/services/User.ts
@@ -10,3 +10,14 @@ export const getUserCommittees = async (): Promise<IRoleInCommittee[]> => {
 	const response = await axios.get('/user/committees')
 	return response.data.committees
 }
+
+export interface IUserProfile {
+	first_name: string
+	last_name: string
+	committees: IRoleInCommittee[]
+}
+
+export const getUserProfile = async (): Promise<IUserProfile> => {
+	const response = await axios.get('/user/profile')
+	return response.data
+}
