5e2dc7d7a3aa634c04a84b718d4a48e553dfcca2
Livia Emilie Valenti Stokke <liviaemilie@yahoo.no>
2022-04-21 00:30:02 +0200

    feat: wipe application data modal
    * notifications on request
    * validation of form
    * handling for no application period

---
+++ frontend/src/components/WipeAdmissionDataModal.tsx
@@ -0,0 +1,192 @@
+import { Box, Button, createStyles, TextInput } from '@mantine/core'
+import { Modal } from '@mantine/core'
+import { useForm } from '@mantine/form'
+import { useNotifications } from '@mantine/notifications'
+import { Check, Trash, X } from 'tabler-icons-react'
+import { wipeApplicationData } from '../services/Applications'
+
+interface IWipeModal {
+	opened: boolean
+	setOpened: (opened: boolean) => void
+}
+
+const useStyles = createStyles((theme) => ({
+	labelText: {
+		color: 'white',
+	},
+	inputField: {
+		backgroundColor: 'transparent',
+		color: 'white',
+		width: '70%',
+	},
+	modal: {
+		backgroundColor: theme.colors.ntnui_background[9],
+		color: 'white',
+		border: '2px solid ' + theme.colors.ntnui_yellow[9],
+		padding: '1rem',
+	},
+	modalTitle: {
+		color: 'white',
+		fontSize: '1rem',
+		h1: {
+			fontWeight: 500,
+			margin: 0,
+		},
+		span: {
+			color: theme.colors.ntnui_red[9],
+		},
+	},
+	modalCloseButton: {
+		alignSelf: 'start',
+		transition: '0.2s',
+		color: theme.colors.ntnui_red[9],
+		':hover': {
+			backgroundColor: theme.colors.ntnui_red[9] + '4D',
+		},
+	},
+	buttonWrapper: {
+		display: 'flex',
+		gap: '1rem',
+		margin: '1rem 0 0 0',
+	},
+	cancelButton: {
+		backgroundColor: theme.colors.ntnui_red[9],
+		transition: '0.3s',
+		border: '2px solid' + theme.colors.ntnui_red[9],
+		fontWeight: 500,
+		':hover': {
+			border: '2px solid' + theme.colors.ntnui_red[9],
+			color: theme.colors.ntnui_red[9],
+			backgroundColor: 'transparent',
+		},
+	},
+	confirmButton: {
+		backgroundColor: theme.colors.ntnui_blue[9],
+		border: '2px solid' + theme.colors.ntnui_blue[9],
+		transition: '0.3s',
+		fontWeight: 500,
+		':hover': {
+			border: '2px solid' + theme.colors.ntnui_blue[9],
+			color: theme.colors.ntnui_blue[9],
+			backgroundColor: 'transparent',
+		},
+	},
+}))
+
+function WipeModal({ opened, setOpened }: IWipeModal) {
+	const { classes } = useStyles()
+	const notifications = useNotifications()
+
+	// Validate input
+	const form = useForm({
+		initialValues: {
+			confirm: '',
+		},
+		validate: {
+			confirm: (value: string) =>
+				value === 'slett' ? undefined : 'Skriv inn "slett"',
+		},
+	})
+
+	const handleWipeApplicationData = async () => {
+		// Wipe app data
+		const notificationId = notifications.showNotification({
+			id: 'wipe-notification',
+			title: 'Sletter opptaksdata...',
+			message: '',
+			loading: true,
+			autoClose: false,
+		})
+		try {
+			await wipeApplicationData()
+			notifications.updateNotification(notificationId, {
+				id: notificationId,
+				loading: false,
+				color: 'green',
+				icon: <Check size={18} />,
+				title: 'Opptaksdata slettet!',
+				message: '',
+				autoClose: 7000,
+			})
+			form.reset()
+		} catch (error: any) {
+			console.log(error.response.status)
+			if (error.response.status === 403) {
+				notifications.updateNotification(notificationId, {
+					id: notificationId,
+					loading: false,
+					color: 'red',
+					icon: <X size={18} />,
+					title: 'Du har ikke rettighetene til å slette opptaksdata!',
+					message: '',
+					autoClose: 6000,
+				})
+			} else {
+				notifications.updateNotification(notificationId, {
+					id: notificationId,
+					loading: false,
+					color: 'red',
+					icon: <X size={18} />,
+					title: 'Noe gikk galt ved sletting av opptaksdata!',
+					message: `En feil oppstod ved sletting. Prøv igjen. Dersom det vedvarer ta kontakt med sprint@ntnui.no`,
+					autoClose: 6000,
+				})
+			}
+		}
+	}
+
+	return (
+		<Modal
+			title={
+				<h1>
+					<span>Slett</span> all opptaksdata
+				</h1>
+			}
+			size='lg'
+			centered
+			opened={opened}
+			onClose={() => setOpened(false)}
+			classNames={{
+				title: classes.modalTitle,
+				close: classes.modalCloseButton,
+				modal: classes.modal,
+			}}
+		>
+			<p>Dette er ment å gjøres når man er ferdig med semesterets opptak.</p>
+			<p>Ved sletting av opptaksdata slettes følgende:</p>
+			<ul>
+				<li>Alle søknader som er sendt inn og tilhørende data</li>
+				<li>Alle kontoer i søknadssystemet som har logget inn</li>
+			</ul>
+
+			<form onSubmit={form.onSubmit(handleWipeApplicationData)}>
+				<TextInput
+					required
+					classNames={{ label: classes.labelText, input: classes.inputField }}
+					label='Skriv “slett” for å bekrefte at du vil slette all opptaksdata'
+					onBlur={() => form.validateField('confirm')}
+					{...form.getInputProps('confirm')}
+				/>
+				<Box className={classes.buttonWrapper}>
+					<Button
+						onClick={() => setOpened(false)}
+						className={classes.cancelButton}
+						leftIcon={<X size={18} />}
+					>
+						Avbryt
+					</Button>
+					<Button
+						type='submit'
+						onClick={handleWipeApplicationData}
+						className={classes.confirmButton}
+						leftIcon={<Trash size={18} />}
+					>
+						Slett all opptaksdata
+					</Button>
+				</Box>
+			</form>
+		</Modal>
+	)
+}
+
+export default WipeModal
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
-import { Box, createStyles, Transition } from '@mantine/core'
+import { Box, Button, createStyles, Transition } from '@mantine/core'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { CalendarEvent, FileText, Users } from 'tabler-icons-react'
+import { CalendarEvent, FileText, Trash, Users } from 'tabler-icons-react'
 import {
 	isApplicationPeriodActive,
 	getAdmissionPeriod,
@@ -9,6 +9,7 @@ import {
 import isOrganizer from '../utils/isOrganizer'
 import { IUserProfile, getUserProfile } from '../services/User'
 import dayjs from 'dayjs'
+import WipeModal from '../components/WipeAdmissionDataModal'
 require('dayjs/locale/nb')
 
 const useStyles = createStyles((theme) => ({
@@ -80,6 +81,20 @@ const useStyles = createStyles((theme) => ({
 	loader: {
 		margin: '0 0 -3px 0',
 	},
+	wipeDataButton: {
+		marginTop: '6rem',
+		backgroundColor: 'transparent',
+		transition: '0.3s',
+		border: '2px solid' + theme.colors.ntnui_red[9],
+		color: theme.colors.ntnui_red[9],
+		fontWeight: 500,
+		textTransform: 'uppercase',
+		':hover': {
+			color: 'white',
+			border: '2px solid' + theme.colors.ntnui_red[9],
+			backgroundColor: theme.colors.ntnui_red[9],
+		},
+	},
 }))
 
 function Dashboard() {
@@ -91,10 +106,11 @@ function Dashboard() {
 	const [isTheOrganizer, setTheOrganizer] = useState<boolean>(false)
 	const [isLoading, setIsLoading] = useState<boolean>(false)
 	const [userName, setUserName] = useState<IUserProfile>()
+	const [wipeModalOpen, setWipeModalOpen] = useState<boolean>(false)
 
 	useEffect(() => {
-		setIsLoading(true)
 		const getDashboardDataAsync = async () => {
+			setIsLoading(true)
 			try {
 				// Get user
 				const user = await getUserProfile()
@@ -126,58 +142,70 @@ function Dashboard() {
 	}, [])
 
 	return (
-		<Box className={classes.dashboardWrapper}>
-			<Transition
-				mounted={!isLoading}
-				transition='fade'
-				duration={100}
-				timingFunction='ease'
-			>
-				{(styles) => (
-					<>
-						<h1 style={styles} className={classes.text}>
-							<span className={classes.header}>
-								<span className={classes.subHeader}>Hei, </span>
-								{userName?.first_name} {userName?.last_name}
-							</span>
-						</h1>
-						<p style={styles} className={classes.text}>
-							{periodOpen ? (
-								<>
-									<CalendarEvent size={24} strokeWidth={1.5} /> Opptaksperioden er satt
-									fra <span className={classes.date}>{startDate}</span> til{' '}
-									<span className={classes.date}>{endDate}</span>
-								</>
-							) : (
-								<span>Det er for tiden ingen satt opptaksperiode</span>
-							)}
-						</p>
-						<div style={styles} className={classes.metroBoxWrapper}>
-							<Box
-								className={classes.metroBoxes}
-								onClick={() => navigate('/applications')}
-							>
-								<FileText size={150} strokeWidth={0.9} /> Søknader
-							</Box>
-							<Box
-								className={classes.metroBoxes}
-								onClick={() => navigate('/admission-status')}
-							>
-								<Users size={150} strokeWidth={0.9} /> Opptaksstatus
-							</Box>
-							{isTheOrganizer && (
+		<>
+			<WipeModal opened={wipeModalOpen} setOpened={setWipeModalOpen} />
+			<Box className={classes.dashboardWrapper}>
+				<Transition
+					mounted={!isLoading}
+					transition='fade'
+					duration={100}
+					timingFunction='ease'
+				>
+					{(styles) => (
+						<>
+							<h1 style={styles} className={classes.text}>
+								<span className={classes.header}>
+									<span className={classes.subHeader}>Hei, </span>
+									{userName?.first_name} {userName?.last_name}
+								</span>
+							</h1>
+							<p style={styles} className={classes.text}>
+								{periodOpen && startDate && endDate ? (
+									<>
+										<CalendarEvent size={24} strokeWidth={1.5} /> Opptaksperioden er satt
+										fra <span className={classes.date}>{startDate}</span> til{' '}
+										<span className={classes.date}>{endDate}</span>
+									</>
+								) : (
+									<span>Det er for tiden ingen satt opptaksperiode</span>
+								)}
+							</p>
+							<div style={styles} className={classes.metroBoxWrapper}>
 								<Box
 									className={classes.metroBoxes}
-									onClick={() => navigate('/admission-period')}
+									onClick={() => navigate('/applications')}
 								>
-									<CalendarEvent size={150} strokeWidth={0.9} /> Opptaksperiode
+									<FileText size={150} strokeWidth={0.9} /> Søknader
 								</Box>
+								<Box
+									className={classes.metroBoxes}
+									onClick={() => navigate('/admission-status')}
+								>
+									<Users size={150} strokeWidth={0.9} /> Opptaksstatus
+								</Box>
+								{isTheOrganizer && (
+									<Box
+										className={classes.metroBoxes}
+										onClick={() => navigate('/admission-period')}
+									>
+										<CalendarEvent size={150} strokeWidth={0.9} /> Opptaksperiode
+									</Box>
+								)}
+							</div>
+							{isTheOrganizer && (
+								<Button
+									onClick={() => setWipeModalOpen(true)}
+									className={classes.wipeDataButton}
+									leftIcon={<Trash size={18} />}
+								>
+									Slett opptaksdata
+								</Button>
 							)}
-						</div>
-					</>
-				)}
-			</Transition>
-		</Box>
+						</>
+					)}
+				</Transition>
+			</Box>
+		</>
 	)
 }
 
