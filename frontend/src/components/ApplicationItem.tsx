1f6000ef1eb162278b267fcea107c7aee43547c2
LiviaValenti <liviaemilie@yahoo.no>
2022-08-19 16:06:35 +0200

    fix: application item styling

--- frontend/src/components/ApplicationItem.tsx
+++ frontend/src/components/ApplicationItem.tsx
@@ -2,80 +2,86 @@ import { Box, createStyles } from '@mantine/core'
 import { useMemo } from 'react'
 import { IApplication, ICommittee } from '../types/types'
 
-const useStyles = createStyles((theme) => ({
-	box: {
-		color: theme.colors.gray[2],
-		border: '2px solid #F8F082',
-		padding: theme.spacing.sm,
-		borderRadius: theme.radius.sm,
-		cursor: 'pointer',
-		display: 'flex',
-		alignItems: 'center',
-		flexDirection: 'row',
-		gap: '0.5rem',
-		transition: 'ease-in-out 0.1s',
-		boxShadow: '0rem 0.2rem 0.4rem ' + theme.colors.dark[7],
+interface IApplicationOverviewStyleProps {
+	idx: number
+}
 
-		'&:hover': {
-			color: '#F8F082',
-			boxShadow: '0 0 5px #F8F082',
+const useStyles = createStyles(
+	(theme, { idx }: IApplicationOverviewStyleProps) => ({
+		box: {
+			color: theme.colors.gray[2],
+			border: '0px solid #F8F082',
+			padding: theme.spacing.sm,
+			borderRadius: theme.radius.sm,
+			cursor: 'pointer',
+			display: 'flex',
+			alignItems: 'center',
+			flexDirection: 'row',
+			gap: '0.5rem',
+			transition: 'ease-in-out 0.1s',
+			backgroundColor: idx % 2 === 0 ? '#F8F0820F' : 'none',
+			'&:hover': {
+				backgroundColor: '#F8F0822D',
+				color: '#F8F082',
+			},
+			'&:active': {
+				color: '#F8F082',
+				boxShadow: '0 0 2px #F8F082',
+			},
 		},
-		'&:active': {
-			color: '#F8F082',
-			boxShadow: '0 0 2px #F8F082',
-		},
-	},
-	grid: {
-		marginLeft: '0',
-		flexGrow: '3',
-		whiteSpace: 'nowrap',
-		fontWeight: '300',
-		fontSize: 'medium',
-		display: 'grid',
-		gap: '1em',
-		gridTemplateColumns: '6fr 10fr 2fr',
-	},
-	nameDiv: {
-		minWidth: '8em',
-		overflow: 'hidden',
-		textOverflow: 'ellipsis',
-	},
-	committeeDiv: {
-		borderLeft: '1px solid #F8F082',
-		paddingLeft: '1rem',
-		overflow: 'hidden',
-		textOverflow: 'ellipsis',
-	},
-	badgeDiv: {
-		display: 'flex',
-		alignContent: 'center',
-		color: theme.colors.ntnui_yellow[9],
-	},
-	date: {
-		color: theme.colors.ntnui_yellow[9],
-		whiteSpace: 'nowrap',
-
-		margin: 'auto',
-		'@media (max-width: 500px)': {
+		grid: {
+			marginLeft: '0',
+			flexGrow: '3',
+			whiteSpace: 'nowrap',
+			fontWeight: '300',
 			fontSize: 'medium',
+			display: 'grid',
+			gap: '1em',
+			gridTemplateColumns: '6fr 10fr 2fr',
 		},
-		'@media (min-width: 500px)': {
-			fontSize: 'medium',
+		nameDiv: {
+			minWidth: '8em',
+			overflow: 'hidden',
+			textOverflow: 'ellipsis',
 		},
-	},
-}))
+		committeeDiv: {
+			borderLeft: '1px solid #F8F082',
+			paddingLeft: '1rem',
+			overflow: 'hidden',
+			textOverflow: 'ellipsis',
+		},
+		badgeDiv: {
+			display: 'flex',
+			alignContent: 'center',
+			color: theme.colors.ntnui_yellow[9],
+		},
+		date: {
+			color: theme.colors.ntnui_yellow[9],
+			whiteSpace: 'nowrap',
+			margin: 0,
+			'@media (max-width: 500px)': {
+				fontSize: 'medium',
+			},
+			'@media (min-width: 500px)': {
+				fontSize: 'medium',
+			},
+		},
+	})
+)
 
 interface IApplicationItem extends IApplication {
+	idx: number
 	handleClick(id: string): void
 }
 function ApplicationItem({
 	_id,
+	idx,
 	name,
 	committees,
 	submitted_date,
 	handleClick,
 }: IApplicationItem) {
-	const { classes } = useStyles()
+	const { classes } = useStyles({ idx })
 
 	const date = new Date(submitted_date)
 
