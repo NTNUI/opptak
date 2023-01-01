1c6926b68e90a22c2a32afb55cfbe5567142071d
eivindkopperud <eivind.kop@gmail.com>
2023-01-01 16:47:00 +0100

    chore: only listen on port non-test env

--- backend/app.ts
+++ backend/app.ts
@@ -48,6 +48,10 @@ app.use(errorHandler)
 
 const port = process.env.PORT || 8082
 
-app.listen(port, () => console.log(`🍑 Express.js app running on port ${port}`))
+if (process.env.NODE_ENV !== 'test') {
+	app.listen(port, () =>
+		console.log(`🍑 Express.js app running on port ${port}`)
+	)
+}
 
 export default app
