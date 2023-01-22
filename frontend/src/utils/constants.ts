c219e1eccf32bafef7d06bfd420e4febfb3d4c5d
Livia Valenti <54808860+LiviaValenti@users.noreply.github.com>
2023-01-22 12:33:48 +0100

    feat: change main board name based on season (#152)
    * Hovedstyret in spring
    * Introstyret in autumn

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -3,3 +3,6 @@ export const REACT_APP_MAIN_BOARD_ID =
 
 export const REACT_APP_ELECTION_COMMITTEE_ID =
 	Number(process.env.REACT_APP_ELECTION_COMMITTEE_ID) || 53
+
+export const MAIN_BOARD_NAME =
+	new Date().getMonth() < 6 ? 'Hovedstyret' : 'Introstyret'
